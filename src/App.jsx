e1b56aec109d430ecb027728f666a87219ac1051
Jatin Foujdar <jatin321001@gmail.com>
2024-04-29 15:35:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+     <img src='./Image/9078.jpg'
+        alt="Background"
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
